35fa21257be896c6a6aa389d423d9d2936dfde9f
Camilo <camilo@camilo.fm>
2020-02-04 20:22:29 -0300

    Removing extra reacts.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 const {useState} = React;
 
 const DoolizLib = () => {
-    // const [state, setState] = useState('foo');
+    const [state, setState] = useState('foo');
 
     return (
         <div>test </div>
